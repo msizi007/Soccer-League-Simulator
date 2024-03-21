f6900fe98de92bfe31ced1adfe0237f611a1a0d4
RINGZ007 <110634670+msizi007@users.noreply.github.com>
2024-03-21 17:33:09 +0000

    updated data format for teams and introduced constants

--- main.py
+++ main.py
@@ -3,56 +3,47 @@ from results import compute_results, print_standings
 from simulate import simulate_season
 from team import Team
 
+ALL_TEAMS = [
+    {"name": "Arsenal", "coefficient": 75},
+    {"name": "Aston Villa", "coefficient": 75},
+    {"name": "Brighton & Hove Albion", "coefficient": 60},
+    {"name": "Burnley", "coefficient": 55},
+    {"name": "Chelsea", "coefficient": 80},
+    {"name": "Crystal Palace", "coefficient": 65},
+    {"name": "Everton", "coefficient": 70},
+    {"name": "Leeds United", "coefficient": 60},
+    {"name": "Liverpool", "coefficient": 90},
+    {"name": "Manchester City", "coefficient": 85},
+    {"name": "Manchester United", "coefficient": 80},
+    {"name": "Newcastle United", "coefficient": 60},
+    {"name": "Norwich City", "coefficient": 50},
+    {"name": "Southampton", "coefficient": 65},
+    {"name": "Tottenham Hotspur", "coefficient": 75},
+    {"name": "Watford", "coefficient": 60},
+    {"name": "West Ham United", "coefficient": 70},
+    {"name": "Wolverhampton Wanderers", "coefficient": 65}
 
-premier_league_teams = [
-    "Arsenal", "Aston Villa", "Brighton & Hove Albion", "Burnley", "Chelsea",
-    "Crystal Palace", "Everton", "Leeds United", "Leicester City", "Liverpool",
-    "Manchester City", "Manchester United", "Newcastle United", "Norwich City",
-    "Nothingham Forest",
-    "Southampton", "Tottenham Hotspur", "Watford", "West Ham United", "Wolverhampton Wanderers"
 ]
 
-team_coefficients = {
-    "Arsenal": 75,
-    "Aston Villa": 50,
-    "Brighton & Hove Albion": 60,
-    "Burnley": 55,
-    "Chelsea": 80,
-    "Crystal Palace": 65,
-    "Everton": 70,
-    "Leeds United": 65,
-    "Leicester City": 75,
-    "Liverpool": 90,
-    "Manchester City": 85,
-    "Manchester United": 80,
-    "Newcastle United": 60,
-    "Norwich City": 50,
-    "Nothingham Forest": 55,
-    "Southampton": 65,
-    "Tottenham Hotspur": 75,
-    "Watford": 60,
-    "West Ham United": 70,
-    "Wolverhampton Wanderers": 65
-}
-
-
 # Create a Team object for each team
-teams = []
-for i, team_name in enumerate(premier_league_teams):
-    team_coefficient = team_coefficients[team_name]
+TEAMS = []
+for team in ALL_TEAMS:
+    # team is a dictionary containing {"name": name, "coefficient": coefficient}
+    team_name =team.get('name')
+    team_coefficient = team.get('coefficient')
     team_squad = generate_squad(team_name, team_coefficient)
     team = Team(team_name, team_coefficient, team_squad)
-    teams.append(team)
+    TEAMS.append(team)
 
 # Print the teams and their squads
-for team in teams:
+for team in TEAMS:
     print(f"{team.name}: {team.coefficient}")
     print("Squad:")
     for player in team.squad:
         print(f"{player.name}: {player.coefficient}")
     print()
 
-matches = simulate_season(teams)
+matches = simulate_season(TEAMS)
 
 team_stats = compute_results(matches)
 print_standings(team_stats)
